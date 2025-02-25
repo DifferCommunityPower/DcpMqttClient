cd36d69ec17be31f86bf203b3135763043085b6b
Espen Øverbye <eo@airmine.ai>
2025-02-25 09:18:12 +0100

    comments, some cleanup, formatting

--- utils.py
+++ utils.py
@@ -1,72 +1,80 @@
 import os
 import subprocess
 import logging
+
 log = logging.getLogger("__name__")
 
-def get_id(flows,label):
+
+def get_id(flows, label):
     for node in flows:
-        nodelabel = node.get('label')
+        nodelabel = node.get("label")
         if nodelabel:
-            nodelabel = nodelabel.split('-')[0]
+            nodelabel = nodelabel.split("-")[0]
         if nodelabel == label:
-            return node.get('id')
-        
+            return node.get("id")
+
+
 def get_labels(flows):
     flows_list = []
     for node in flows:
-        label = node.get('label')
+        label = node.get("label")
         if label:
             flows_list.append(label)
     return flows_list
 
 
 def getVersion() -> str:
-    filename = os.path.join(os.path.abspath(os.path.dirname(__file__)),"version")
+    filename = os.path.join(os.path.abspath(os.path.dirname(__file__)), "version")
     with open(filename) as f:
-        return f.read().replace('\n','')
+        return f.read().replace("\n", "")
+
 
 def put_pw_nr(password):
-    log.debug(f'Setting Password to {password}')
-    with open('/data/conf/dcppassword.txt','w') as f:
+    log.debug(f"Setting Password to {password}")
+    with open("/data/conf/dcppassword.txt", "w") as f:
         f.write(password)
+    # great here with a comment to why we are doing this, setting the env variable to /home/root
     os.environ["HOME"] = "/home/root"
-    r = subprocess.run("node-red admin hash-pw",input=str(password),shell=True,capture_output=True,text=True)
+    r = subprocess.run(
+        "node-red admin hash-pw",
+        input=str(password),
+        shell=True,
+        capture_output=True,
+        text=True,
+    )
     hash = r.stdout.split()[1]
     log.debug(r)
-    with open('/data/conf/vncpassword.txt','w') as f:
+    with open("/data/conf/vncpassword.txt", "w") as f:
         f.write(hash)
-    subprocess.run('killall node-red',shell=True)
+    subprocess.run("killall node-red", shell=True)
+
 
 def get_pw_nr():
-    with open('/data/conf/dcppassword.txt','r') as f:
+    with open("/data/conf/dcppassword.txt", "r") as f:
         return f.read()
+
 
 def get_errors_nr():
     logs: list[str] = []
-    lines:list[str] = []
-    with open('/data/log/node-red-venus/current','r') as f:
-        for line in f:
-            lines.append(line)
-    for i in range(len(lines)):
-        line = lines[-1-i]
+    lines = _get_logs("/data/log/node-red-venus/current")
+    for line in lines:
         log.info(f"Checking line for error:{line}")
-        if line.find("error")!=-1:
+        if line.find("error") != -1:
             logs.append(line)
-        elif line.find("Starting")!=-1:
+        elif line.find("Starting") != -1:
             break
-    
+
     return logs
+
+
+def _get_logs(filename: str):
+    with open(filename, "r") as f:
+        return f.readlines()
+
 
 def get_logs_nr():
-    logs = []
-    with open('/data/log/node-red-venus/current','r') as f:
-        for line in f:
-            logs.append(line)
-    return logs
+    return _get_logs("/data/log/node-red-venus/current")
+
 
 def get_logs_dcp():
-    logs = []
-    with open('/data/log/DcpMqttClient/current','r') as f:
-        for line in f:
-            logs.append(line)
-    return logs
+    return _get_logs("/data/log/DcpMqttClient/current")
